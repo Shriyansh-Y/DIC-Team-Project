a5b5b8a9c6d07201e0c3b324f70eb53f15755e19
nsingh9 <nsingh9@ncsu.edu>
2017-12-04 03:32:36 +0000

    adding batch file

---
+++ batch_process.py
@@ -0,0 +1,42 @@
+from pyspark.context import SparkContext
+from pyspark import SparkConf
+from pyspark.sql import Row
+from pyspark.sql import HiveContext
+from pyspark.sql import DataFrame
+
+sc = SparkContext.getOrCreate()
+hiveCtx = HiveContext(sc)
+
+
+
+
+def extract_hashtag(ht):
+	ht_clean = ht[5:-5].encode('utf-8').decode('ascii','ignore')
+	hashtags = []
+	for each in ht_clean.split(","):
+		hashtags.append(each.split(":")[1][:-1])
+	return hashtags
+
+def check_sentiment(tweet):
+    return [1,0]
+
+
+text = sc.hadoopFile('abc.txt', "org.apache.hadoop.mapred.TextInputFormat", "org.apache.hadoop.io.Text", "org.apache.hadoop.io.LongWritable")
+
+
+textmap = text.map(lambda x: (extract_hashtag(x[1].split("\x03")[1]), (x[1].split("\x03")[3].split('{')[1][5:-2]).encode('utf-8').decode('ascii','ignore')))
+
+tweetmap = textmap.map(lambda x: (x[0], check_sentiment(x[1])))
+
+hashtagmap = tweetmap.flatMap(lambda x: ((y[1:-1],x[1]) for y in x[0] ))
+
+r = hashtagmap.map(lambda x: x).reduceByKey(lambda m, n: [m[0] + n[0], m[1] + n[1]])
+
+# Create dataframe and hive table
+df = r.map(lambda x: Row(hashtag=x[0], pos=x[1][0], neg=x[1][1])).toDF(['hashtag', 'pos', 'neg'])
+df.registerTempTable("temp_t")
+aggRDD = hiveCtx.sql("select * from temp_t")
+aggRDD.write.saveAsTable("test_result")
+
+
+textmap.saveAsTextFile('test/test1.csv')
